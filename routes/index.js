89cd26c67116a2d43078bb6a7e5ed644db3888e0
Tin Vo <tinnvo1101@gmail.com>
2017-04-25 03:58:10 -0500

    Changed to request-promise

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
-var request = require('request');
-var cheerio = require('cheerio')
+var rp = require('request-promise');
+var cheerio = require('cheerio');
 var router = express.Router();
 
 /* GET home page. */
@@ -9,26 +9,38 @@ router.get('/', function(req, res, next) {
 });
 
 /* GET homepage with id */
-router.get('/:id', function(req, res, next) {
+router.get('/songs/:id', function(req, res, next) {
     var results = [Object];
-    var songs = [Object];
-    var playlists = [Object];
-    request('http://m.nhaccuatui.com/tim-kiem/bai-hat?q=' + req.params.id, function (error, response, body) {
-        const $ = cheerio.load(body);
+    rp('http://m.nhaccuatui.com/tim-kiem/bai-hat?q=' + req.params.id)
+    .then(function (htmlString) {
+        const $ = cheerio.load(htmlString);
         $('.bgmusic h3 a').each(function(i, elem) {
             var link = $(this).attr('href');
             var title = $(this).attr('title')
-            songs[i] =  {
+            results[i] =  {
                 link : link,
                 title: title
             }
         });
-        results = {
-            'songs': songs,
-            'playlists': playlists
-        }
         res.send(results);
+    })
+    .catch(function (err) {
+        console.log(err);
     });
+
+
+    // request('http://m.nhaccuatui.com/tim-kiem/bai-hat?q=' + req.params.id, function (error, response, body) {
+    //     const $ = cheerio.load(body);
+    //     $('.bgmusic h3 a').each(function(i, elem) {
+    //         var link = $(this).attr('href');
+    //         var title = $(this).attr('title')
+    //         songs[i] =  {
+    //             link : link,
+    //             title: title
+    //         }
+    //     });
+    //     res.send(results);
+    // });
 });
 
 module.exports = router;
