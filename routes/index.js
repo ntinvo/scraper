3b8773db405f34aaab27d9677fde5af2b182b8df
Tin Vo <tinnvo1101@gmail.com>
2017-04-26 10:44:26 -0500

    Updated routes

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,60 @@ router.get('/', function(req, res, next) {
     res.render('index');
 });
 
+
+
+
+
+
+
+
+
+
+/*######################################################################*/
+/************************************************************************/
+/*                                CSN                                   */
+/************************************************************************/
+/*######################################################################*/
+// /* GET songs with key words */
+// router.get('/csn/songs/:id', function(req, res, next) {
+//     var results = [Object];
+//     rp('http://search.chiasenhac.vn/search.php?s=' + req.params.id)
+//     .then(function (htmlString) {
+//         const $ = cheerio.load(htmlString);
+//         $('.tenbh').each(function(i, elem) {
+//             if(i > 0) {
+//                 console.log($(this).$('.tenbh p a'));
+//             }
+//             // var link = $(this).children('a').attr('href');
+//             // var title = $(this).children('a').attr('title');
+//             // var artist = $(this).siblings('p').children('img').attr('alt');
+//             // var index = link.lastIndexOf('/');
+//             // link = link.substring(index + 1);
+//             // results[i] =  {
+//             //     link : link,
+//             //     title: title,
+//             //     artist: artist
+//             // }
+//         });
+//         res.send(htmlString);
+//     })
+//     .catch(function (err) {
+//         console.log(err);
+//     });
+// });
+
+
+
+
+
+/*######################################################################*/
+/************************************************************************/
+/*                                NCT                                   */
+/************************************************************************/
+/*######################################################################*/
+
 /* GET songs with key words */
-router.get('/songs/:id', function(req, res, next) {
+router.get('/nct/songs/:id', function(req, res, next) {
     var results = [Object];
     rp('http://m.nhaccuatui.com/tim-kiem/bai-hat?q=' + req.params.id)
     .then(function (htmlString) {
@@ -18,8 +70,8 @@ router.get('/songs/:id', function(req, res, next) {
             var link = $(this).children('a').attr('href');
             var title = $(this).children('a').attr('title');
             var artist = $(this).siblings('p').children('img').attr('alt');
-            var index = link.lastIndexOf('/');
-            link = link.substring(index + 1);
+            // var index = link.lastIndexOf('/');
+            // link = link.substring(index + 1);
             results[i] =  {
                 link : link,
                 title: title,
@@ -34,7 +86,7 @@ router.get('/songs/:id', function(req, res, next) {
 });
 
 /* GET playlists with key words */
-router.get('/playlists/:id', function(req, res, next) {
+router.get('/nct/playlists/:id', function(req, res, next) {
     var results = [Object];
     rp('http://m.nhaccuatui.com/tim-kiem/playlist?q=' + req.params.id)
     .then(function (htmlString) {
@@ -58,17 +110,15 @@ router.get('/playlists/:id', function(req, res, next) {
 });
 
 /* GET specific song with url */
-router.get('/getSong/:id', function(req, res, next) {
+router.get('/nct/getSong/:id', function(req, res, next) {
     var results = [Object];
-    var songUrl = 'http://m.nhaccuatui.com/bai-hat/' + req.params.id;
-    rp(songUrl)
+    rp(req.params.id)
     .then(function (htmlString) {
         const $ = cheerio.load(htmlString);
         var link = $('.download p a').attr('href');
         var title = $('.player-song h2 img').attr('alt');
         var artist = $('.player-song p img').attr('alt');
         var lyric = $('.lyric').text();
-        // lyric = lyric.replace('\n', '<br />')
         results[0] = {
             link: link,
             title: title,
