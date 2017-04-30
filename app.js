15756805ef2d80c6ef535b6c39acfd1189085942
Tin Vo <tinnvo1101@gmail.com>
2017-04-29 20:56:39 -0500

    Added CORS

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cors = require('express-cors');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -16,23 +17,27 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(cors({
+    allowedOrigins: [
+        'github.com', 'google.com', 'http://localhost:8080/'
+    ]
+}))
 app.use('/', index);
 app.use('/users', users);
 
-// Add headers
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
+// add headers
+// app.use(function (req, res, next) {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//     res.header('Access-Control-Allow-Headers', 'content-type');
+//     next();
+// });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
